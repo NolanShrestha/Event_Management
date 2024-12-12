a6c4d8d52c926ce24315316b0c9e5bb2fe917be4
Nolan Shrestha <96360893+NolanShrestha@users.noreply.github.com>
2024-12-12 18:55:05 +0545

    Minor change

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/createEvent', createEvent);
-router.post('/getEvents', getEvents);
+router.get('/getEvents', getEvents);
 router.patch('/editEvent', editEvent);
 router.delete('/deleteEvent', deleteEvent);
 router.post('/bookEvent', bookEvent);
